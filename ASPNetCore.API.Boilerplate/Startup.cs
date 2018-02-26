b95c1b615d794eee26a51e00f7a0088e75295a76
c4bo3l <c4bo3l@gmail.com>
2018-02-26 23:27:15 +0800

    [+] Cache [+] Rate limit [+] API Documentation

--- ASPNetCore.API.Boilerplate/Startup.cs
+++ ASPNetCore.API.Boilerplate/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using ASPNetCore.API.Boilerplate.DTOs;
 using ASPNetCore.API.Boilerplate.Models;
 using ASPNetCore.API.Boilerplate.Repositories;
+using AspNetCoreRateLimit;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,8 +42,49 @@ namespace ASPNetCore.API.Boilerplate
             });
 
             services.AddDbContext<MyDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
-
+            services.AddScoped<IUrlHelper, UrlHelper>(factory => {
+                ActionContext actionContext = factory.GetService<IActionContextAccessor>().ActionContext;
+                return new UrlHelper(actionContext);
+            });
             services.AddTransient<CompanyRepository>();
+
+            services.AddResponseCaching();
+
+            services.AddHttpCacheHeaders(expirationOption => {
+                expirationOption.MaxAge = 600;
+            }, validationOption => {
+                validationOption.AddMustRevalidate = true;
+            });
+
+            services.AddMemoryCache();
+
+            services.Configure<IpRateLimitOptions>(options => {
+                options.GeneralRules = new System.Collections.Generic.List<RateLimitRule>() {
+                    // All IPs allow to request 1000 times in 5 minutes
+                    new RateLimitRule(){
+                        Endpoint="*",
+                        Limit=1000,
+                        Period="5m"
+                    }
+                    // All IPs only allow send two requests in ten seconds
+                    //,new RateLimitRule(){
+                    //    Endpoint="*",
+                    //    Limit=2,
+                    //    Period="10s"
+                    //}
+                };
+            });
+
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("libraryapi", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "ASP.NET Core API Documentation",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,7 +105,16 @@ namespace ASPNetCore.API.Boilerplate
                     });
             });
 
+            app.UseIpRateLimiting();
+
+            app.UseResponseCaching();
+            app.UseHttpCacheHeaders();
+
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint(Configuration["Swagger:LibJsonPath"], "Library API");
+            });
         }
     }
 }
